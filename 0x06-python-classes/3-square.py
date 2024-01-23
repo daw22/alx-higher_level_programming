8b0012473cecdf26d30679537245ee7f3c9e47ff
Dawit Yifru <dawitxxd@gmail.com>
2024-01-23 18:01:01 +0300

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 3-square.py by Dawit yifru
+"""Define square class"""
+
+class Square:
+    """Define a class for a square with an
+        area method"""
+    def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2

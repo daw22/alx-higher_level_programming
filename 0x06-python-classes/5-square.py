e0af410924cafd0ea01a4145785918152482ce7e
Dawit Yifru <dawitxxd@gmail.com>
2024-01-23 21:23:14 +0300

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# 5-square.py by Dawit Yifru
+""" Define a sqare class """
+
+
+class Square:
+    """A square class with getter, setter and publicly
+        available area and my_ptint methods"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size mist be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")

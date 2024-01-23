f963c9618b1c32950fb915895bd77e769cc799cf
Dawit Yifru <dawitxxd@gmail.com>
2024-01-23 23:24:01 +0300

    Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+# 6-square.py by Dawit Yifru
+"""
+defines a square
+"""
+
+
+class Square:
+    """
+    Defines a square class with getter, setters and
+    other methods
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an int")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+        for i in range(self.__position[1]):
+            print("")
+        for j in range(self.__size):
+            for k in range(self.__position[0]):
+                print(" ", end="")
+            for m in range(self.__size):
+                print("#", end="")
+            print("")

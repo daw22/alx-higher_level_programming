70f5e18ee33c25945de14cd633ebd5e0b25df0af
Dawit Yifru <dawitxxd@gmail.com>
2024-01-26 00:40:56 +0300

    Divide a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# 2-matrix_divided.py by Dawit Yifru
+"""
+module containing a matrix divider
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    divides a matrix with div and returns
+    a new matrix
+    """
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    r_size = len(matrix[0])
+    for i in range(len(matrix)):
+        if len(matrix[i]) != r_size:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    for i in range(len(matrix)):
+        for j in range(r_size):
+            if not isinstance(matrix[i][j], (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists)"
+                                " of integers/floats")
+
+    result = []
+    for i in range(len(matrix)):
+        tmp = []
+        for j in range(r_size):
+            tmp.append(round(matrix[i][j] / div, 2))
+        result.append(tmp)
+
+    return result

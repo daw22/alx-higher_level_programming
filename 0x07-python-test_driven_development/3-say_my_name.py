11f9fe2428a69ab3b0aeb11b78de3e5be1013b6c
Dawit Yifru <dawitxxd@gmail.com>
2024-01-26 21:51:18 +0300

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+# 3-say_my_name.py by Dawit Yifru
+"""
+module with a function that prints out given
+first and last name
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    prints:
+        My name is {first_name} {last_name}
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))

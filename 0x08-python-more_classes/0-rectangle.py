a7fc027af373f6c50744b0c0e317a8e7af6730da
Dawit Yifru <dawitxxd@gmail.com>
2024-01-29 09:19:33 +0300

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 0-rectangle.py by Dawit Yifru
+"""
+A Basic class Defination
+
+"""
+
+
+def Rectangle:
+    """
+    An empty class that represents a rectangle
+    """
+    pass

74c1fbd28b8360d5b51036e331db9ba04a608202
Dawit Yifru <dawitxxd@gmail.com>
2024-02-05 18:39:35 +0300

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+contains an empty geometry class
+"""
+
+
+class BaseGeometry:
+    """
+    an empty BaseGeometry class
+
+    """
+    def area(self):
+        """
+        area of the geometry
+        """
+        raise Exception("area() is not implemented")

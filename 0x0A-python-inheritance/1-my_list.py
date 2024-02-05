3f77c1b1a4d7763f178a8f83cdb3ebdba9e0875b
Dawit Yifru <dawitxxd@gmail.com>
2024-02-05 12:38:53 +0300

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-calss the inherits from list
+class the inherits from list
 
 """
 
 
 class MyList(list):
     """
-    inherits from list and add fuctionality
+    inherits from list and adds a method
     """
     def print_sorted(self):
         """

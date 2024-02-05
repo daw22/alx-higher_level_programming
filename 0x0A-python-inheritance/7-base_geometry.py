99a1f5ede148c2bb418b0e9674359c939283e3d3
Dawit Yifru <dawitxxd@gmail.com>
2024-02-05 21:23:05 +0300

    Validate integer

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+contains a basic BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    A basic gometry class with area and
+    integer_validator public methods
+    """
+    def area(self):
+        """
+        calculates area of a geometic shape
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates a value to be a positive integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

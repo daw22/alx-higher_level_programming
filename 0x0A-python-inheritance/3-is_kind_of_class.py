691eafca1152d514e54823ccb723ad3998949016
Dawit Yifru <dawitxxd@gmail.com>
2024-02-05 11:42:34 +0300

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contains function the checks id an object is an
+instance of ....
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks if obj is an istance of a_class
+    """
+    return isinstance(obj, a_class)

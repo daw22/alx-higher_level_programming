aade285400e565fa5c829c9a02025e0a077a77a3
Dawit Yifru <dawitxxd@gmail.com>
2024-02-06 21:34:10 +0300

    Student to Json with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -20,7 +20,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         returns a dictionary representation of a student
         object if attrs is listof strings that 

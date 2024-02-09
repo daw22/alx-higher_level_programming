28a4a3ec9374986d9108fbc3582ea9e70572aa02
Dawit Yifru <dawitxxd@gmail.com>
2024-02-09 13:31:53 +0300

    base

---
+++ 0x0c-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+base.py
+"""
+
+
+class Base():
+    """
+    This is a Base class
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        A constrictor for Base class
+        Args:
+            id: id's an object
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

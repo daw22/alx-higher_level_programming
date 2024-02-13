d1a3e2dbc60114eced4d1d844cbd02713a12338f
Dawit Yifru <dawitxxd@gmail.com>
2024-02-13 13:10:54 +0300

    init

--- 0x0C-python-almost_a_circle/models/__init__.py
+++ 0x0C-python-almost_a_circle/models/__init__.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+"""
+models module - contains base, rectangle, and square
+classes
+    Base: the base class every class in the module inherits
+    from
+    Rectangle: a class representing a rectangle
+    Square: a special class inheriting Rectangle
+"""

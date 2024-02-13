7b6719d59d29b68301dacd98a129bc71bfd12af5
Dawit Yifru <dawitxxd@gmail.com>
2024-02-13 09:22:52 +0300

    based doc

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,11 +17,7 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        A constrictor for Base class
-        Args:
-            id: id's an object
-        """
+        """  """
         if id is not None:
             self.id = id
         else:

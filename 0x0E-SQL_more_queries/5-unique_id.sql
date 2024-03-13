3d32bc8e7570cd50a70a2b92b3034dbb474e77a3
Dawit Yifru <dawitxxd@gmail.com>
2024-03-13 22:56:05 +0300

    Unique id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- creates a table 'unique_id' with unique id column
 
-CREATE TABLE IF NOT EXIST unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXIST unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));

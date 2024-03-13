1736f2a18a9cebf75ba1326586158b2b086ede64
Dawit Yifru <dawitxxd@gmail.com>
2024-03-13 23:49:35 +0300

    Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genere_id_by_show.sql
@@ -0,0 +1,5 @@
+-- lists all shows contained in hbtn_od_tvshows that have at least one gener linked
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_shows_genres ON tv_shows.id=tv_shows_genres.show_id
+ORDER BY tv_shows.title, tv_shows_genres.genre_id;

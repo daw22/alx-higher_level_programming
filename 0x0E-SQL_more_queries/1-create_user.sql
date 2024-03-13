546a2bf6d7b246f3d6d4b08e1ce62a5be0a994e2
Dawit Yifru <dawitxxd@gmail.com>
2024-03-13 22:21:22 +0300

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- creates user 'user_od_1' if user doesn't exist
 -- password should be set to 'user_0d_1_pwd'
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

02c40270d7bcc8bdc4535b0e70eed0b9c45ae3cf
Dawit Yifru <dawitxxd@gmail.com>
2024-03-13 22:30:15 +0300

    create read user

--- 0x0E-SQL_more_queries/2-creare_read_user.sql
+++
@@ -1,8 +0,0 @@
--- creates a dtabase 'hbtn_od_2' if it doesn't exist
--- creates a user 'user_0d_2' if it doesn't exist
--- grants only select privilege for 'user_0d_2'
--- set 'user_0d_2' password to 'user_0d_2_pwd'
-
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';

981da42cd3afcabe89fe4761a6dd278a38f7b2fc
Dawit Yifru <dawitxxd@gmail.com>
2024-04-11 21:23:16 +0300

    filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+where there names start with 'N'
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """ main script """
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE name = %s
+              ORDER BY id ASC""", (sys.argv[4], ))
+    results = c.fetchall()
+    for item in results:
+        print(item)
+    c.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()

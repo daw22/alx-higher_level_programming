a200c52f2d7cd46822c043114a8a67b67b97798f
Dawit Yifru <dawitxxd@gmail.com>
2024-04-11 18:31:24 +0300

    Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """ main script """
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY id ASC""")
+    results = c.fetchall()
+    for item in results:
+        print(item)
+    c.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()

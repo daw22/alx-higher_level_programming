8a31fe18e6c274447f62ce8e35f886692894bae6
Dawit Yifru <dawitxxd@gmail.com>
2024-04-12 10:27:22 +0300

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,8 +17,9 @@ def main():
             passwd=sys.argv[2],
             db=sys.argv[3])
     c = db.cursor()
-    c.execute("""SELECT * FROM states WHERE name = '{:s}'
-              ORDER BY id ASC""".format(sys.argv[4]))
+    c.execute("""SELECT * FROM states
+                 WHERE name LIKE BINARY '{:s}'
+                 ORDER BY id ASC""".format(sys.argv[4]))
     results = c.fetchall()
     for item in results:
         print(item)

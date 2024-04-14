478766422c507a359d59a977687749bdab5e7e9d
Dawit Yifru <dawitxxd@gmail.com>
2024-04-14 13:16:42 +0300

    List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    
+
     states = session.query(State).order_by(State.id).all()
 
     for state in states:

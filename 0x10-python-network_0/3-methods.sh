c8b2759ddebc346cb35dabca762fdf1e4c95b404
Dawit Yifru <dawitxxd@gmail.com>
2024-04-25 16:36:05 +0300

    make it silent fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #list all allowed methods of a url
-
-curl -X OPTIONS -sI "$1" | grep -i Allow | cut -d' ' -f2-
+curl -s -X OPTIONS "$1" | grep -i Allow | cut -d' ' -f2-

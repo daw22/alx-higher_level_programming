2a34410a8284e7011adc89a35154d5526b2de3a7
Dawit Yifru <dawitxxd@gmail.com>
2024-04-25 10:19:15 +0300

    cURL body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# a bash script that takes a URL, sends a request to that URL
+#- and display the size of the body of the responce
+
+curl -sI 0:5000 | grep -i content-Length | cur -d' ' -f2-

6466c08de57a7e9be0506dc113d1d2afdf832ec0
Dawit Yifru <dawitxxd@gmail.com>
2024-04-25 18:08:44 +0300

    Post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends data along with request to the server
-curl -s -X POST -d "email=test@gmail.com&subject=I eill always be there for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be there for PLD" "$1"

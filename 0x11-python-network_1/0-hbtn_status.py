6c57f958217139f773ca8a8d26503dfa8ae209cb
Dawit Yifru <dawitxxd@gmail.com>
2024-04-26 12:33:48 +0300

    typo fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as responce:
         body = responce.read()
-    print("Body response")
+    print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
     print("\t- utf8 content:", body.decode('utf-8'))

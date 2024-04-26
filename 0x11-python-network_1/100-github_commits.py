eecd3b3732a977b8d52a9110b576162a355e5abd
Dawit Yifru <dawitxxd@gmail.com>
2024-04-26 21:56:09 +0300

    Time for interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Task #100 - Time for an interview!
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(repo, owner)
+    r = requests.get(url)
+    commits = r.json()
+    for i in range(0, 10):
+        commit = commits[i]
+        print("{}: {}".format(commit.get("sha"),
+                              commit.get("commit", {})
+                              .get("author", {}).get("name")))

89c6c318075148485a9960c892d0ad4b18a4dcfa
Dawit Yifru <dawitxxd@gmail.com>
2024-05-21 13:50:46 +0300

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log('code:', response && response.statuscode);
+    console.log('code:', response && response.statusCode);
   }
 });

cf701ac46a3d46c7e58ac3f5f42eabbeadd2f5e6
Dawit Yifru <dawitxxd@gmail.com>
2024-05-21 18:47:27 +0300

    characters movie count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,16 +7,14 @@ let count = 0;
 
 request(url, function (error, response, body) {
   if (!error && response.body) {
-    const movies = JSON.parse(body).results;
-    for (const movie in movies) {
-      const m = movies[movie];
-      for (const character in m.characters) {
-        const c = m.characters[character];
-        if (c.includes('18')) {
+    const data = JSON.parse(body);
+    data.results.forEach((movie) => {
+      movie.characters.forEach((character) => {
+        if (character.includes('18')) {
           count++;
         }
-      }
-    }
+      });
+    });
     console.log(count);
   } else {
     console.log(error);

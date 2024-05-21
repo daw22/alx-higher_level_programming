87ae3d4b0e38d47266822ab421f273d089fc0eda
Dawit Yifru <dawitxxd@gmail.com>
2024-05-21 20:19:38 +0300

    responce to file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,9 +5,13 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 const fs = require('fs');
 
-request(url, function (error, response, body) {
-  if (!error && body) {
-    fs.writeFileSync(filePath, body, { encoding: 'utf8' });
+request(url, (error, response, body) => {
+  if (!error) {
+    fs.writeFile(filePath, body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   } else {
     console.log(error);
   }

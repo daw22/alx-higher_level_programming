79917970fa949f66c6c8aa4532cd9a05d467720b
Dawit Yifru <dawitxxd@gmail.com>
2024-05-30 09:49:44 +0300

    say hello to everyone!

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$('document').ready(function () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?';
+  $('INPUT#btn_translate').on('click', function () {
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+      $('DIV#hello').html(data.hello);
+    });
+  });
+});
